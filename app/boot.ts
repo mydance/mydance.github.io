8413c122a269874fee9d0adeb8502685ea64dde9
Sewon Ahn <sewonist@gmail.com>
2017-03-20 16:49:06 +0900

    m

--- app/boot.ts
+++ app/boot.ts
@@ -1,3 +1,5 @@
+import {} from 'angular2/core';
+
 import {bootstrap}  from 'angular2/platform/browser'
 import {SimpleComponent} from './simple.component'
  
--- app/simple.component.ts
+++ app/simple.component.ts
@@ -3,7 +3,13 @@ import {Component} from "angular2/core"
 @Component({
     selector: "simple-component",
     template: `
- 
+ <link rel="stylesheet" type="text/css"
+href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css">
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.js"></script>
+<div class="ui container">
 <h1>Hallo Angular 2!</h1>
  
  
@@ -12,9 +18,11 @@ import {Component} from "angular2/core"
  
 <div>
                 <input [(ngModel)]="name" />
-                <button (click)="toggleCalling()">toggle Calling</button>
+                <button (click)="toggleCalling()" class="ui button">toggle Calling</button>
  
 <div>
+        </div>
+        
         </div>
  
         `
