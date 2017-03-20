e973928ab0e4e67b13daedde7174738e047ed41b
Sewon Ahn <sewonist@gmail.com>
2017-03-20 15:26:16 +0900

    Init typescript project.

---
+++ app/simple.component.ts
@@ -0,0 +1,29 @@
+import {Component} from "angular2/core"
+ 
+@Component({
+    selector: "simple-component",
+    template: `
+ 
+<h1>Hallo Angular 2!</h1>
+ 
+ 
+<div>
+            <label [hidden]="!isCalling">name: {{name}}.</label>
+ 
+<div>
+                <input [(ngModel)]="name" />
+                <button (click)="toggleCalling()">toggle Calling</button>
+ 
+<div>
+        </div>
+ 
+        `
+})
+export class SimpleComponent {
+    private name = "da";
+    private isCalling = true;
+ 
+    private toggleCalling() {
+        this.isCalling = !this.isCalling;
+    }
+}
