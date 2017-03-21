7ddaf64f091569ba72a263a9eb325372ce33c0c6
Sewon Ahn <sewonist@gmail.com>
2017-03-21 19:06:06 +0900

    m

--- app/simple.component.ts
+++ app/simple.component.ts
@@ -1,5 +1,5 @@
-import {Component} from "angular2/core"
-import {Http, HTTP_BINDINGS} from 'angular2/http';
+import {Component} from "angular2/core";
+import {Http} from "angular2/http";
  
 @Component({
     selector: "simple-component",
