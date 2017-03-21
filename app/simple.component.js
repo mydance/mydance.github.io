ee6257fa0778b7e04ec2e54fc44228d11ad39f54
Sewon Ahn <sewonist@gmail.com>
2017-03-21 18:52:31 +0900

    m

--- app/simple.component.js
+++ app/simple.component.js
@@ -1,4 +1,4 @@
-System.register(["angular2/core"], function (exports_1, context_1) {
+System.register(["angular2/core", "angular2/http"], function (exports_1, context_1) {
     "use strict";
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -6,22 +6,34 @@ System.register(["angular2/core"], function (exports_1, context_1) {
         else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
         return c > 3 && r && Object.defineProperty(target, key, r), r;
     };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
     var __moduleName = context_1 && context_1.id;
-    var core_1, SimpleComponent;
+    var core_1, http_1, SimpleComponent;
     return {
         setters: [
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (http_1_1) {
+                http_1 = http_1_1;
             }
         ],
         execute: function () {
             SimpleComponent = (function () {
-                function SimpleComponent() {
-                    this.name = "da";
-                    this.isCalling = true;
+                function SimpleComponent(http) {
+                    var _this = this;
+                    this.http = http;
+                    this.club = "da";
+                    this.nickname = true;
+                    http.get('http://itpointlab.cafe24.com/mydance/playlist')
+                        .map(function (res) { return res.json(); })
+                        .subscribe(function (data) { return _this.data = data; }, function (err) { return console.log(err); }, function () { return _this.update(); });
                 }
-                SimpleComponent.prototype.toggleCalling = function () {
-                    this.isCalling = !this.isCalling;
+                SimpleComponent.prototype.update = function () {
+                    this.club = this.data.info.club;
+                    console.log('Completed');
                 };
                 return SimpleComponent;
             }());
@@ -29,7 +41,8 @@ System.register(["angular2/core"], function (exports_1, context_1) {
                 core_1.Component({
                     selector: "simple-component",
                     templateUrl: './templates/simple.component.html',
-                })
+                }),
+                __metadata("design:paramtypes", [http_1.Http])
             ], SimpleComponent);
             exports_1("SimpleComponent", SimpleComponent);
         }
--- app/simple.component.ts
+++ app/simple.component.ts
@@ -1,14 +1,25 @@
 import {Component} from "angular2/core"
+import {Http, HTTP_BINDINGS} from 'angular2/http';
  
 @Component({
     selector: "simple-component",
     templateUrl: './templates/simple.component.html',
 })
 export class SimpleComponent {
-    private name = "da";
-    private isCalling = true;
- 
-    private toggleCalling() {
-        this.isCalling = !this.isCalling;
+    private club = "da";
+    private nickname = true;
+    private data;
+     
+    constructor(private http: Http) {
+      http.get('http://itpointlab.cafe24.com/mydance/playlist')
+        .map(res => res.json())
+        .subscribe(data => this.data = data,
+                    err => console.log(err),
+                    () => this.update());
+    }
+    
+    update(){
+        this.club = this.data.info.club;
+        console.log('Completed');
     }
 }
