9d614e9195e05b9a7c9e5d3f32ae1bfa6278b542
Sewon Ahn <sewonist@gmail.com>
2017-03-23 14:22:38 +0900

    m

--- app/simple.component.js
+++ app/simple.component.js
@@ -1,4 +1,4 @@
-System.register(["angular2/http", "angular2/core", "rxjs/add/operator/map"], function (exports_1, context_1) {
+System.register(["angular2/http", "angular2/core", "rxjs/add/operator/map", "rxjs/Rx"], function (exports_1, context_1) {
     "use strict";
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -10,7 +10,7 @@ System.register(["angular2/http", "angular2/core", "rxjs/add/operator/map"], fun
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var __moduleName = context_1 && context_1.id;
-    var http_1, core_1, SimpleComponent;
+    var http_1, core_1, Rx_1, Song, SimpleComponent;
     return {
         setters: [
             function (http_1_1) {
@@ -20,24 +20,50 @@ System.register(["angular2/http", "angular2/core", "rxjs/add/operator/map"], fun
                 core_1 = core_1_1;
             },
             function (_1) {
+            },
+            function (Rx_1_1) {
+                Rx_1 = Rx_1_1;
             }
         ],
         execute: function () {
+            Song = (function () {
+                function Song() {
+                }
+                return Song;
+            }());
             SimpleComponent = (function () {
                 function SimpleComponent(http) {
                     var _this = this;
+                    this.interval = 2000;
                     this.club = "da";
-                    this.nickname = true;
+                    this.list = [];
                     console.log(http);
-                    this.data = http.get('https://itpointlab.cafe24.com/mydance/playlist')
+                    this.http = http;
+                    this.getData();
+                    this.now = new Song();
+                    Rx_1.Observable
+                        .interval(this.interval)
+                        .subscribe(function () { return _this.getData(); });
+                }
+                SimpleComponent.prototype.getData = function () {
+                    var _this = this;
+                    this.data = this.http.get('https://itpointlab.cafe24.com/mydance/playlist')
                         .map(function (res) { return res.json(); })
                         .subscribe(function (data) { return _this.data = data; }, function (err) { return console.log(err); }, function () { return _this.update(); });
-                }
-                SimpleComponent.prototype.toggleCalling = function () {
                 };
                 SimpleComponent.prototype.update = function () {
                     this.club = this.data.info.club;
-                    console.log('Completed');
+                    this.nickname = this.data.info.nickname;
+                    for (var i = 0; i < this.data.list.length; i++) {
+                        this.data.list[i].length = this.prettyTime(this.data.list[i].length);
+                    }
+                    this.now = this.data.list[0];
+                    this.list = this.data.list.slice(1, this.data.list.length);
+                };
+                SimpleComponent.prototype.prettyTime = function (sec) {
+                    var date = new Date(null);
+                    date.setSeconds(sec); // specify value for SECONDS here
+                    return date.toISOString().substr(14, 5);
                 };
                 return SimpleComponent;
             }());
--- app/simple.component.ts
+++ app/simple.component.ts
@@ -1,33 +1,68 @@
 import {Http, HTTP_PROVIDERS} from 'angular2/http'
 import {Component} from "angular2/core";
 import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/Rx';
 
+class Song  {
+    public album;
+    public artist;
+    public title;
+    public tracknumber;
+    public length;
+    public genere;
+    public date;
+    public bitrate;
+    public codec;
+}
+    
 @Component({
     selector: "simple-component",
     templateUrl: './templates/simple.component.html',
     providers: [],
 })
 export class SimpleComponent {
-    private club = "da";
-    private nickname = true;
-    private data;
+    
+    interval = 2000;
+    club = "da";
+    nickname;
+    data;
+    http;
+    list:Object[] = [];
+    now:Song;
     
     constructor(http:Http) {
         console.log(http);
+        this.http = http;
+        this.getData();
+        this.now = new Song();
         
-        this.data = http.get('https://itpointlab.cafe24.com/mydance/playlist')
+        Observable
+            .interval(this.interval)
+            .subscribe(() =>this.getData());
+    }
+    
+    getData(){
+        this.data = this.http.get('https://itpointlab.cafe24.com/mydance/playlist')
             .map(res => res.json())
             .subscribe(data => this.data = data,
                         err => console.log(err),
                         () => this.update());
     }
     
-    private toggleCalling(){
-        
-    }
-    
     update(){
         this.club = this.data.info.club;
-        console.log('Completed');
+        this.nickname = this.data.info.nickname;
+        for (let i = 0; i < this.data.list.length; i++) {
+            this.data.list[i].length = this.prettyTime(this.data.list[i].length);
+        }
+        this.now = this.data.list[0];
+        this.list = this.data.list.slice(1,this.data.list.length);
     }
+    
+    prettyTime(sec){
+        var date = new Date(null);
+        date.setSeconds(sec); // specify value for SECONDS here
+        return date.toISOString().substr(14, 5);
+    }
+    
 }
